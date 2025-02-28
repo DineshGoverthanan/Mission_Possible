1ec3745e1812206cb46528d57ac91b2807a30c68
DineshGoverthanan <dinesh.goverthanan@kone.com>
2025-02-28 09:15:26 +0530

    Update kpi.py

--- kpi.py
+++ kpi.py
@@ -57,6 +57,7 @@ Testrun_filter_id = 6261
 Defect_filter_id = 6017
 client_ID = os.getenv("JAMA_CLIENT_ID")
 client_Secret = os.getenv("JAMA_CLIENT_SECRET")
+print("##########################################################################",client_ID,client_Secret)
 client = JamaClient(
     host_domain=host_domain,
     credentials=(client_ID, client_Secret),
